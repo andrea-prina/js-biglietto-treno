ba82652f6ede784e44102e918617c6170983fd0a
andrea-prina <and.prina@gmail.com>
2022-06-14 15:58:57 +0200

    Esercizio completato

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
-// Realizziamo un programma che dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero o della passeggera.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-//      il prezzo del biglietto è definito in base ai km (0.21 € al km)
-//      va applicato uno sconto del 20% per i minorenni
-//      va applicato uno sconto del 40% per gli over 65.
-// L'output del prezzo finale andrà mostrato in forma umana in console (con massimo due decimali, per indicare centesimi sul prezzo).
-
 // Ask the user to input the number of km they want to travel
-const travelKms = parseInt(prompt("Indica quanti km vuoi percorrere: "));
+const travelKms = parseInt(prompt("Indica quanti km vuoi percorrere (valore intero): "));
 const userAge = parseInt(prompt("Inserisci (in numero) la tua età: "));
 
-console.log(travelKms, typeof travelKms, userAge, typeof userAge);
+// Define the variabile to calculate the price of the ticket. It is defined as a global variable to be more accessible from other eventual features
+let ticketPrice;
+
+// Check if the inputs are valid numbers 
+if (isNaN(travelKms) || isNaN(userAge)) {
+    console.log("Impossibile calcolare prezzo poichè i dati non sono stati inseriti correttamente")
+
+} else {
+
+    // The base price of the ticket is equal to 0.21€ for each km
+    ticketPrice = travelKms * 0.21;
+
+    // Check if the userAge is < 18 so they will be eligible for the 20% discount or > 65 so they will be eligible for the 40% discount
+    if (userAge < 18) {
+        ticketPrice = ticketPrice * (1 - 0.2);
+    
+    } else if (userAge >= 65) {
+        ticketPrice = ticketPrice * (1 - 0.4);
+    
+    }
+
+    // Print the final tiket price to console.
+    console.log(`Il prezzo del tuo biglietto è pari a: ${ticketPrice.toFixed(2)}€`)
+
+}
+
+
+// ! The toFixed(n) method rounds the number to the nth decimal but converts it to a string
+// To preserve the number type use the formula Math.round((num + Number.EPSILON) * 100) / 100; where Number.EPSILON is a very small number added to ensure an accurate rounding.
+// ticketPrice = Math.round((ticketPrice + Number.EPSILON) * 100) / 100;
+// console.log(typeof ticketPrice);
+// console.log(Number.EPSILON);
+
